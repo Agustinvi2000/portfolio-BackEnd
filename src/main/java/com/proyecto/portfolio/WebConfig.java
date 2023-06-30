e5d107dbe4de27331d886d2e0807bd5e7f16fff8
Agustinvi2000 <agustinvi2000@hotmail.com>
2023-06-29 21:05:59 -0300

    Prueba CORS 2

--- src/main/java/com/proyecto/portfolio/WebConfig.java
+++ src/main/java/com/proyecto/portfolio/WebConfig.java
@@ -16,6 +16,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 @Override
 public void addCorsMappings(CorsRegistry registry) {
-registry.addMapping("/**");
+registry.addMapping("/**")
+                .allowedOrigins("https://portfolio-avi.web.app") // Reemplaza con tu dominio permitido
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type", "Access-Control-Allow-Origin")
+                .allowCredentials(true);
 }
 }
